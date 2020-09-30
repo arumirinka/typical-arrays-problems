019aa7f75f95e5a2672dbed0f5f7194a4394af50
Irina Stepanova <irina.s.vesna@mail.ru>
2020-09-30 22:03:29 +0600

    feat: add TAP-task solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+  return array && array.length > 0 ? array.sort((a, b) => a - b).shift() : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  return array && array.length > 0 ? array.sort((a, b) => a - b).pop() : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array && array.length > 0 ? array.reduce((acc, val) => acc + val) / array.length : 0;
 }
